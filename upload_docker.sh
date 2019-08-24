cd6ab2dfaec3314817c3081e1562c6cf3737b055
bauercole <bauer.cole@gmail.com>
2019-08-23 23:23:38 -0500

    Tasks 1-4

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=udacitydevops:predict
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=bauercole
 
 # Step 3:
 # Push image to a docker repository
+docker push bauercole/$dockerpath
