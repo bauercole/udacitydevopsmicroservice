49761a0f639e7ea2897c8d5a968f291148e6307e
bauercole <bauer.cole@gmail.com>
2019-08-24 13:13:28 -0500

    Task 6

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=bauercole/udacitydevops:predict
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacitypredict --image=$dockerpath --port 80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/udacitypredict 8000:80
